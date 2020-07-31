305293867cf9f5ab2f2f1b0afd89202bbb2da068
Acrylic125 <acrylic125email@gmail.com>
2020-07-31 09:00:20 +0800

    Morphia and MongoDB support.

--- AcrylicUtils/src/main/java/com/acrylic/acrylicutils/Commands/TestCommand.java
+++ AcrylicUtils/src/main/java/com/acrylic/acrylicutils/Commands/TestCommand.java
@@ -1,19 +1,22 @@
 package com.acrylic.acrylicutils.Commands;
 
 import com.acrylic.acrylicutils.AcrylicUtils;
-import com.acrylic.version_1_8.EntityAnimator.EntityEquipment;
-import com.acrylic.version_latest.Animations.HandRotatorAnimation;
-import com.acrylic.version_latest.Animations.ItemDangle;
-import com.acrylic.version_latest.EntityAnimators.Entities.GiantAnimator;
-import com.acrylic.version_latest.EntityAnimators.EntityAnimator;
-import com.acrylic.version_latest.Utils.Time;
+import com.acrylic.acrylicutils.Commands.Test.EntityTest;
+import com.acrylic.version_latest.Items.ItemCreator;
+import com.mongodb.*;
+import dev.morphia.Datastore;
+import dev.morphia.Key;
+import dev.morphia.Morphia;
+import dev.morphia.dao.BasicDAO;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
-import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
 
 public class TestCommand implements CommandExecutor{
 
@@ -22,11 +25,34 @@ public class TestCommand implements CommandExecutor{
 
         Player p = (Player) sender;
 
-        Location center = p.getLocation().clone();
 
-        Time time = new Time().setColorScheme("&6","&6");
-        Bukkit.broadcastMessage(time.convert(Double.parseDouble(args[0])));
 
+        /**MongoClient client = AcrylicUtils.getMongoClient();
+        Morphia morphia = new Morphia().map(EntityTest.class);
+
+        EntityTest test = new EntityTest("Test","TFJ","TVFV",100);
+
+        //test.setAddress(new ItemCreator(Material.ICE));
+        Bukkit.broadcastMessage("Test 2");
+        Datastore datastore = morphia.createDatastore(client,"Database");
+        datastore.ensureIndexes();
+        Bukkit.broadcastMessage("Test 3");
+        datastore.save(test);
+        Bukkit.broadcastMessage("Test CCC");
+        /**MongoDatabase testDB = client.getDatabase(args[0]);
+        Bukkit.broadcastMessage("Test " + testDB);
+        try {
+            testDB.createCollection(args[1]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        MongoCollection<Document> docs = testDB.getCollection(args[1]);
+        Document document = new Document("Test",1).append("Lol",new ItemCreator(Material.DIAMOND_SWORD).getItem()).append("Sjitf",new ArrayList<>());
+        docs.insertOne(document);
+
+        Bukkit.broadcastMessage("Test 2");
+
+        Bukkit.broadcastMessage(testDB.getName() + "");
         /**ItemDangle itemDangle = new ItemDangle(p);
         for (int i = 1;i<=Integer.parseInt(args[0]);i++) {
             EntityAnimator entityManager = new GiantAnimator(center);
