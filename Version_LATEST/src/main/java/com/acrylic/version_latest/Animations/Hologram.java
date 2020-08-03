f782f418e023cef9000da927c041e16939deb473
Acrylic125 <acrylic125email@gmail.com>
2020-08-03 17:59:22 +0800

    Holograms

---
+++ Version_LATEST/src/main/java/com/acrylic/version_latest/Animations/Hologram.java
@@ -0,0 +1,46 @@
+package com.acrylic.version_latest.Animations;
+
+import com.acrylic.version_latest.EntityAnimators.Entities.ArmorStandAnimator;
+import com.acrylic.version_latest.Utils.Teleport;
+import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+
+@Getter
+public class Hologram {
+
+    private final float offsetHeight;
+    private final ArmorStand hologram;
+
+    public Hologram(Location location, int index, String hologramText, float yOffset) {
+        offsetHeight = (index * Holograms.OFFSET_HEIGHT) + yOffset;
+        ArmorStandAnimator armorStandAnimator = new ArmorStandAnimator(location.clone().add(0,offsetHeight,0)).setSmall();
+        hologram = armorStandAnimator.getEntity();
+        hologram.setCustomName(hologramText);
+        setup();
+    }
+
+    public Hologram(Location location, int index, ArmorStand hologram, float yOffset) {
+        offsetHeight = (index * Holograms.OFFSET_HEIGHT) + yOffset;
+        Teleport.tp(hologram,location.clone().add(0,offsetHeight,0));
+        this.hologram = hologram;
+        setup();
+    }
+
+    private void setup() {
+        hologram.setCustomNameVisible(true);
+        hologram.setMarker(true);
+    }
+
+    /**
+     * RAW USE! Do not use this method unless you are using it for NMS
+     * entities.
+     * @param location The location where you want to teleport the INDIVIDUAL
+     *                 hologram armor stands to.
+     */
+    public void teleport(Location location) {
+        Teleport.tp(hologram,location.getX(),location.getY() + offsetHeight, location.getZ());
+    }
+
+
+}
--- Version_LATEST/src/main/java/com/acrylic/version_latest/Animations/Holograms.java
+++ Version_LATEST/src/main/java/com/acrylic/version_latest/Animations/Holograms.java
@@ -1,15 +1,9 @@
 package com.acrylic.version_latest.Animations;
 
-import com.acrylic.version_latest.EntityAnimators.Entities.ArmorStandAnimator;
-import com.acrylic.version_latest.EntityAnimators.EntityAnimator;
 import com.acrylic.version_latest.Messages.ChatUtils;
-import com.acrylic.version_latest.Utils.Teleport;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.LivingEntity;
-
 import java.util.LinkedList;
 
 public class Holograms {
@@ -17,7 +11,7 @@ public class Holograms {
     /**
      * This is the offset height between each hiologram armor stand.
      */
-    private final static float OFFSET_HEIGHT = 0.25f;
+    protected final static float OFFSET_HEIGHT = 0.25f;
 
     @Getter
     private LinkedList<Hologram> holograms = new LinkedList<>();
@@ -77,42 +71,5 @@ public class Holograms {
         hologram.getHologram().remove();
     }
 
-    @Getter
-    public final static class Hologram {
-
-        private final float offsetHeight;
-        private final ArmorStand hologram;
-
-        public Hologram(Location location, int index, String hologramText, float yOffset) {
-            offsetHeight = (index * OFFSET_HEIGHT) + yOffset;
-            ArmorStandAnimator armorStandAnimator = new ArmorStandAnimator(location.clone().add(0,offsetHeight,0)).setSmall();
-            hologram = armorStandAnimator.getEntity();
-            hologram.setCustomName(hologramText);
-            setup();
-        }
-
-        public Hologram(Location location, int index, ArmorStand hologram, float yOffset) {
-            offsetHeight = (index * OFFSET_HEIGHT) + yOffset;
-            Teleport.tp(hologram,location.clone().add(0,offsetHeight,0));
-            this.hologram = hologram;
-            setup();
-        }
-
-        private void setup() {
-            hologram.setCustomNameVisible(true);
-            hologram.setMarker(true);
-        }
-
-        /**
-         * RAW USE! Do not use this method unless you are using it for NMS
-         * entities.
-         * @param location The location where you want to teleport the INDIVIDUAL
-         *                 hologram armor stands to.
-         */
-        public void teleport(Location location) {
-            Teleport.tp(hologram,location.getX(),location.getY() + offsetHeight, location.getZ());
-        }
-
-    }
 
 }
